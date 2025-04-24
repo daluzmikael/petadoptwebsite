47c10b356c3caaad8423afb072b9f038b893a95c
Konrad Koc <konrad.koc@uconn.edu>
2025-04-24 13:06:09 -0400

    pylint changes

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,19 @@
+"""Main Api Function"""
 from flask import Flask
 from user_api import user_api
 from pet_api import pet_api
-from extra_api import extra_api   
+from extra_api import extra_api
 
 app = Flask(__name__)
 
 app.register_blueprint(user_api)
 app.register_blueprint(pet_api)
-app.register_blueprint(extra_api) 
+app.register_blueprint(extra_api)
 
 @app.route('/')
 def home():
+    """Api home"""
     return 'API is running'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
